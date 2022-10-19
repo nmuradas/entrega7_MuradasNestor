1306c22833d56788ea44aa9661553686193f6f3e
nmuradas <nestormuradas@gmail.com>
2022-10-18 22:05:28 -0300

    Primer commit

---
+++ src/contenedor.js
@@ -0,0 +1,213 @@
+const fs = require("fs");
+
+class Contenedor {
+  constructor(fileName, keys) {
+    this._filename = fileName;
+    this._keys = [...keys, "id"];
+  }
+  
+  _validateKeysExist(newData) {
+    
+    const objectKeys = Object.keys(newData);
+    let exists = true;
+    
+    objectKeys.forEach((key) => {
+      if(!this._keys.includes(key)) {
+        exists = false;
+        
+      }
+    })
+    return exists;
+    
+  }
+
+
+  async getById(id) {
+    id = Number(id);
+    try {
+      const data = await this.getData();
+      const parsedData = JSON.parse(data);
+
+      return parsedData.find((producto) => producto.id === id);
+    } catch (error) {
+      console.log(
+        `Error Code: ${error.code} | Ocurrió un error al intentar obtener un elemento por su ID (${id})`
+      );
+    }
+  }
+
+  async deleteById(id) {
+    try {
+      id = Number(id);
+      const data = await this.getData();
+      const parsedData = JSON.parse(data);
+      const objectIdToBeRemoved = parsedData.find(
+        (producto) => producto.id === id
+      );
+
+      if (objectIdToBeRemoved) {
+        const index = parsedData.indexOf(objectIdToBeRemoved);
+        parsedData.splice(index, 1);
+        await fs.promises.writeFile(this._filename, JSON.stringify(parsedData));
+        return true;
+      } else {
+        console.log(`ID ${id} no existe en el archivo`);
+        return null;
+      }
+    } catch (error) {
+      console.log(
+        `Error Code: ${error.code} | Ocurrió un error al intentar eliminar un elemento por ID (${id})`
+      );
+    }
+  }
+
+  async updateById(id, newData) {
+    if(this._validateKeysExist(newData)){
+      try {
+        id = Number(id);
+        const data = await this.getData();
+        const parsedData = JSON.parse(data);
+        const objectIdToBeUpdated = parsedData.find(
+          (producto) => producto.id === id
+        );
+
+        if (objectIdToBeUpdated) {
+          const index = parsedData.indexOf(objectIdToBeUpdated);
+          Object.keys(objectIdToBeUpdated).forEach( (key) => {
+            parsedData[index][key] = newData[key];
+          })
+    
+          await fs.promises.writeFile(this._filename, JSON.stringify(parsedData));
+          return true;
+        } else {
+          console.log(`ID ${id} no existe en el archivo`);
+          return null;
+        }
+  
+      } catch (error) {
+        `Error Code: ${error.code} | Ocurrió un error al intentar actualizar un elemento por ID (${id})`
+        console.log('error')
+      }
+    } else {
+      console.log('false')
+      return false;
+    }
+  
+    
+  }
+  
+  async addToArrayById(id, objectToAdd) {
+    if(this._validateKeysExist(objectToAdd)) {
+    try {
+      id = Number(id);
+      const data = await this.getData();
+      const parsedData = JSON.parse(data);
+      const objectIdToBeUpdated = parsedData.find(
+        (producto) => producto.id === id
+      );
+      if (objectIdToBeUpdated) {      
+        const index = parsedData.indexOf(objectIdToBeUpdated);
+        const valorActual = parsedData[index];
+        const currentProducts = valorActual['products']
+        currentProducts.push(objectToAdd.products)
+        
+        await fs.promises.writeFile(this._filename, JSON.stringify(parsedData));
+        return true;
+      } else {
+        console.log(`ID ${id} does not exist in the file`);
+        return false;
+      }
+
+    } catch (error) {
+      `Error Code: ${error.code} | Ocurrió un error al intentar actualizar un elemento por ID (${id})`
+    }
+    } else {
+      return false;
+    }
+  }
+
+  async removeFromArrayById(id, objectToRemoveId, keyName) {
+    try {
+      id = Number(id);
+      const data = await this.getData();
+      const parsedData = JSON.parse(data);
+      
+      const objectIdToBeUpdated = parsedData.find(
+        (producto) => producto.id === id
+      );
+      
+      if (objectIdToBeUpdated) {
+        const index = parsedData.indexOf(objectIdToBeUpdated);
+        
+        const valorActual = parsedData[index][keyName];
+        let indexToBeRemoved = -1;
+        valorActual.forEach((element, indexE) => {
+          if(element.id == objectToRemoveId) {
+            indexToBeRemoved = indexE
+          }
+        })
+        const newArray = [...valorActual];
+        
+        if (indexToBeRemoved>-1) {
+          console.log(indexToBeRemoved)
+          newArray.splice(indexToBeRemoved,1)
+        }
+    
+        parsedData[index][keyName] = newArray;
+        await fs.promises.writeFile(this._filename, JSON.stringify(parsedData));
+        return true;
+      } else {
+        console.log(`ID ${id} no existe en el archivo`);
+        return false;
+      }
+
+    } catch (error) {
+      `Error Code: ${error.code} | Ocurrió un error al intentar actualizar un elemento por ID (${id})`
+    }
+  
+  }
+
+  async save(object) {    
+    if(this._validateKeysExist(object)) {
+      try {
+        const allData = await this.getData();
+        const parsedData = JSON.parse(allData);
+  
+        object.id = parsedData.length + 1;
+        parsedData.push(object);
+  
+        await fs.promises.writeFile(this._filename, JSON.stringify(parsedData));
+        return object.id;
+      } catch (error) {
+        console.log(
+          `Error Code: ${error.code} | Ocurrió un error al tratar de guardar un elemento`
+        );
+      }
+    } else {
+      return false;
+    }
+    
+  }
+
+  async deleteAll() {
+    try {
+      await this._createEmptyFile();
+    } catch (error) {
+      console.log(
+        `Ocurrió un error (${error.code}) al tratar de eliminar todos los objetos`
+      );
+    }
+  }
+
+  async getData() {
+    const data = await fs.promises.readFile(this._filename, "utf-8");
+    return data;
+  }
+
+  async getAll() {
+    const data = await this.getData();
+    return JSON.parse(data);
+  }
+}
+
+module.exports = Contenedor;
---
+++ src/server.js
@@ -0,0 +1,148 @@
+const express = require('express');
+const app = express();
+const Contenedor = require('./contenedor')
+const contenedor = new Contenedor("productos.json", ["timestamp", "title", "price", "description", "code", "image", "stock"]);
+const carrito = new Contenedor("carrito.json", ["timestamp", "products"])
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+const authMiddleware = (req, res, next) => {
+    if(req.headers.rol == "admin"){
+        next()
+    }else{
+        res.json({"error": "unauthorized"})}
+}
+
+const routerProducts = express.Router();
+const routerCart = express.Router();
+
+app.use('/api/productos', routerProducts);
+app.use('/api/carrito', routerCart);
+
+/* ------------------------ Productos ------------------------ */
+
+routerProducts.get('/', async (req, res) => {
+    const products = await contenedor.getAll();
+    res.status(200).json(products);
+})
+
+
+routerProducts.get('/:id', async (req, res) => {
+    const { id } = req.params;
+    const product = await contenedor.getById(id);
+    
+    product
+        ? res.status(200).json(product)
+        : res.status(400).json({"error": "Producto no encontrado"})
+})
+
+
+routerProducts.post('/',authMiddleware, async (req,res, next) => {
+    const {body} = req;
+    
+    body.timestamp = Date.now();
+    
+    const newProductId = await contenedor.save(body);
+    
+    newProductId
+        ? res.status(200).json({"success" : "Se ha agredado el producto con el ID: "+newProductId})
+        : res.status(400).json({"error": "Key inválida, compuebe la información"})
+})
+
+
+routerProducts.put('/:id', authMiddleware ,async (req, res, next) => {
+    const {id} = req.params;
+    const {body} = req;
+    const wasUpdated = await contenedor.updateById(id,body);
+    
+    wasUpdated
+        ? res.status(200).json({"success" : "Producto actualizado"})
+        : res.status(404).json({"error": "Producto no encontrado"})
+})
+
+
+routerProducts.delete('/:id', authMiddleware, async (req, res, next) => {
+    const {id} = req.params;
+    const wasDeleted = await contenedor.deleteById(id);
+    
+    wasDeleted 
+        ? res.status(200).json({"success": "Producto exitosamente eliminando"})
+        : res.status(404).json({"error": "Producto no encontrado"})
+})
+
+/* ------------------------ Carrito ------------------------ */
+
+
+
+routerCart.post('/', async(req, res) => {
+    const {body} = req;
+    
+    body.timestamp = Date.now();
+    body.products = [];
+    const newCartId = await carrito.save(body);
+    
+    newCartId
+        ? res.status(200).json({"success" : "Se ha agregado el carrito con el ID: "+newCartId})
+        : res.status(400).json({"error": "Key inválida, compuebe la información"})
+    
+})
+
+
+routerCart.delete('/:id', async (req, res) => {
+    const {id} = req.params;
+    const wasDeleted = await carrito.deleteById(id);
+    
+    wasDeleted 
+        ? res.status(200).json({"success": "Carrito exitosamente eliminado"})
+        : res.status(404).json({"error": "Carrito no encontrado"})
+})
+
+
+routerCart.post('/:id/productos', async(req,res) => {
+    const {id} = req.params;
+    const { body } = req;
+    
+    const product = await contenedor.getById(body['id']);
+    
+    if (product) {
+        const cartExist = await carrito.addToArrayById(id, {"products": product});
+        cartExist
+            ? res.status(200).json({"success" : "Producto añadido"})
+            : res.status(404).json({"error": "Carrito no encontrado"})
+    } else {
+        res.status(404).json({"error": "Producto no encontrado, verifique el ID."})
+    }
+})
+
+
+routerCart.get('/:id/productos', async(req, res) => {
+    const { id } = req.params;
+    const cart = await carrito.getById(id)
+    
+    cart
+        ? res.status(200).json(cart.products)
+        : res.status(404).json({"error": "Carrito no enontrado"})
+})
+
+
+routerCart.delete('/:id/productos/:id_prod', async(req, res) => {
+    const {id, id_prod } = req.params;
+    const productExists = await contenedor.getById(id_prod);
+    if (productExists) {
+        const cartExists = await carrito.removeFromArrayById(id, id_prod, 'products')
+        cartExists
+            ? res.status(200).json({"success" : "Producto eliminado"})
+            : res.status(404).json({"error": "Carrito no enontrado"})
+    } else {
+        res.status(404).json({"error": "Producto no encontrado"})
+    }
+})
+
+const PORT = process.env.PORT || 8080;
+const server = app.listen(PORT, () => {
+console.log(` Server running http://localhost:${PORT}`)
+})
+
+server.on('error', (err) => console.log(err));
